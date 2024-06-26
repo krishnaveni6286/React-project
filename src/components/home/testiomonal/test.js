9e6a3b9b79e59a20b9230cdb1eac953ace3142dd
Your Name <krishnaveni6477@gmail.com>
2024-06-26 18:04:15 +0530

    responsive

--- src/components/home/testiomonal/test.js
+++ src/components/home/testiomonal/test.js
@@ -7,18 +7,18 @@ const Test = () =>{
     return (
         <>
          <section className="testimonal padding">
-            <div className="container">
+            <div className="container1">
                 <Title subtitle="testimonal" title="our successful students"/>
 
-                <div className="content grid2">
+                <div className="content grid21">
                     {testimonal.map((val) => (
-                        <div className="items shadow">
-                            <div className="box flex">
-                                <div className="img">
+                        <div className="items1 shadow1">
+                            <div className="box1 flex1">
+                                <div className="img1">
                                     <img src={val.cover} alt="" />
                                     <i className="fa fa-quote-left icon"></i>
                                 </div>
-                                <div className="name">
+                                <div className="name1">
                                     <h2>{val.name}</h2>
                                     <span>{val.post}</span>
                                 </div>
