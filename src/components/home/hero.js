eb7d187633ea2148b99ca15dd6df18021f107f31
Your Name <krishnaveni6477@gmail.com>
2024-08-12 18:50:37 +0530

    tab responsive

--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -12,7 +12,7 @@ const Hero =()=>{
             <div className="container">
                 <div className="row">
                     <Title subtitle='WELCOME TO ACDAEMIA' title="Best Online Education"/>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus nam, consectetur expedita rem quae est. Nam commodi maxime possimus. Nihil molestias alias quasi qui eligendi facilis expedita id recusandae corrupti.</p>
+                    <p style={{color:"white"}}>Our online course stands out as the best because it combines expert-led instruction with a flexible, allowing you to learn at your own pace. With practical exercises, and personalized support, we ensure to gain the skills to apply it in real-world scenarios. Join us and experience a transformative learning journey that empowers you to achieve your goals and excel in your field..</p>
                  <div className="button">
                     <button className="primary-btn">
                        GET STARTED NOW <i className="fa fa-long-arrow-alt-right"></i>
